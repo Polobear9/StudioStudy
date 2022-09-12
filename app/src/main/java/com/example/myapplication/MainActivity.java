30c66ef0025a37804d7986574dce8a989170e176
Polobear9 <lieams1996@gmail.com>
2022-09-12 15:54:58 +0900

    change the layout more modern.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,22 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.bt_toPost);
-        button.setOnClickListener(this);
-
-
-    }
-
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(this, MainActivity2.class);
-        startActivity(intent);
-        Toast.makeText(this, "move", Toast.LENGTH_SHORT).show();
     }
 }
