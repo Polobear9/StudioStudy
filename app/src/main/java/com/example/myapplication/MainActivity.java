8b1e7155fecf48a9632e2112403d88e2eee80d49
Polobear9 <lieams1996@gmail.com>
2022-09-12 18:57:03 +0900

    set a toast when i click the some icon.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,11 +8,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.bt_toHome).setOnClickListener(this);
+        findViewById(R.id.iv_like).setOnClickListener(this);
+        findViewById(R.id.iv_save).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+            case R.id.iv_like:
+                Toast.makeText(this, "Like this write!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.iv_save:
+                Toast.makeText(this, "Saved!!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bt_toHome:
+                Intent intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
+                Toast.makeText(this, "go to more info!!", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -8,22 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity2 extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_post);
-        Button button = findViewById(R.id.bt_toHome);
-        button.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        Toast.makeText(this, "return to main", Toast.LENGTH_SHORT).show();
-
     }
 }
